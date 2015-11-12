fba607c30da49bdf2545adba2cc147b472a99841
xiaobena <xiaobena@live.cn>
2015-11-12 15:29:25 +0800

    write into csv
    1. 每写一行会产生一个空白行
    2. closed_by 都是空白
    3. pull request, user, labels 需要拆分
    4. comments, events 需要提取

--- getIssue_libsaas.py
+++ getIssue_libsaas.py
@@ -1,17 +1,34 @@
 '''
 Created on 20151111
 
-@author: Lenovo
+@author: xiaobena
 '''
-
+import csv
 from libsaas.services import github
 
 basic = github.Github('username', 'passport')
 
 repo = basic.repo('numpy', 'numpy')
 
-for i in range(1,101):
-    issues = repo.issues().get(state='closed', page = i)
-    for issue in issues:
-        print ('issue: ', issue)
+headers = ['number', 'id', 'created_at', 'updated_at', 'closed_at', 'state', 
+           'locked', 'assignee', 'milestone', 'comments', 'title', 'pull_request',
+           'user','closed_by', 'labels', 'html_url', 'labels_url', 'url', 'events_url', 
+           'comments_url', 'body']
+
+with open('F:/stocks.csv','w') as f:
+    f_csv = csv.DictWriter(f, headers)
+    f_csv.writeheader()
+    
+    for i in range(1,2):
+        issues = repo.issues().get(state='closed', page = i)
+        for issue in issues:
+            try:
+                print ('issue: ', issue)
+                f_csv.writerow(issue)
+            except:
+                print(issue['number'])
+
+
+    
+
 
