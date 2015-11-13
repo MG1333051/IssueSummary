08a3442243fb3c12835eb8eb17c0c8ad2bd95aed
xiaobena <xiaobena@live.cn>
2015-11-13 11:02:55 +0800

    修改输出

--- getIssue_libsaas.py
+++ getIssue_libsaas.py
@@ -6,16 +6,16 @@ Created on 20151111
 import csv
 from libsaas.services import github
 
-basic = github.Github('username', 'passport')
+basic = github.Github('username', 'password')
 
 repo = basic.repo('numpy', 'numpy')
 
-headers = ['number', 'id', 'created_at', 'updated_at', 'closed_at', 'state', 
-           'locked', 'assignee', 'milestone', 'comments', 'title', 'pull_request',
-           'user','closed_by', 'labels', 'html_url', 'labels_url', 'url', 'events_url', 
+headers = ['number', 'id', 'reporter', 'created_at', 'updated_at', 'closed_at', 'state', 
+           'locked', 'assignee', 'milestone', 'comments', 'label_name', 'title', 'pull_request',
+           'user', 'labels', 'html_url', 'labels_url', 'url', 'events_url', 'diff', 'patch',
            'comments_url', 'body']
 
-with open('G:/stocks.csv','wb') as f:
+with open('F:/numpy.csv','w',newline='') as f:
     f_csv = csv.DictWriter(f, headers)
     f_csv.writeheader()
     
@@ -23,12 +23,32 @@ with open('G:/stocks.csv','wb') as f:
         issues = repo.issues().get(state='closed', page = i)
         for issue in issues:
             try:
-                print ('issue: ', issue)
-                f_csv.writerow(issue)
+#                print('issue: ', issue)
+                reporter = issue['user']['login']
+                
+                label_names = []
+                for label in issue['labels']:
+                    label_names.append(label['name'])
+                sep =';'
+                label_name =  sep.join(label_names)
+                
+                diff = ''
+                patch = ''                
+                if 'pull_request' in issue.keys():
+                    diff = issue['pull_request']['diff_url']
+                    patch = issue['pull_request']['patch_url']
+                
+                issue_part = {'reporter':reporter, 'label_name':label_name,
+                              'diff':diff, 'patch':patch}
+                
+                issue_all = {}
+                issue_all.update(issue)
+                issue_all.update(issue_part)
+                
+                f_csv.writerow(issue_all)
+                
             except Exception as e:
-                print(issue['number'], ': ', e)
-
-
-    
-
-
+                print(issue['number'], ': ', e)                
+               
+                
+                
